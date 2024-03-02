d8e26dfb2ed5ba8304bb6b14887725501265837c
Alishba-khann <161767135+Alishba-khann@users.noreply.github.com>
2024-03-02 22:27:27 +0500

    Add files via upload

---
+++ 05.Famous_Quote_2.ts
@@ -0,0 +1,4 @@
+let famous_Person :string = 'Thomas Edison';
+let message : string = 'Our gretest weakness lies in giving up.The most certain way to succeed is always to try just one more time';
+
+console.log(`${famous_Person}once said,"${message}"`);
